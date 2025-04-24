dc6dfa9837b128d06053badf3da047b0a1591478
abu-bakrrd <gaupovar@gmail.com>
2025-04-25 00:19:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Я жив!"
+
+def run():
+    app.run(host='0.0.0.0', port=3000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 import os
 import json
 import uuid
